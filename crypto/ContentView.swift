dc2fe972df5697337d6cc5fdcf7ef33ba5f2e305
baptisteDULIEUX <baptiste.dulieux25420@gmail.com>
2025-01-14 09:51:46 +0100

    voila

--- crypto/ContentView.swift
+++ crypto/ContentView.swift
@@ -9,13 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("Crypto App")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).bold()
+                    .foregroundStyle(Color.green)
+                
+                HStack{
+                    Button{
+                        print("calendrier ouvert")
+                    } label:{
+                        Image(systemName: "calendar")
+                            .frame(width:30, height: 30)
+                            .background(Color.green)
+                            .cornerRadius(25)
+                    }
+                    
+                    Text("Monday September 13, 2025")
+                        .font(.title2)
+                        .foregroundStyle(Color.white)
+                }
+                
+                Spacer()
+                
+                HStack{
+                    cryptoValueInfo(cryptoName: BIT, cryptoImage: bitcoin, cryptoValue: 51)
+                }
+                
+                
+            }
         }
-        .padding()
+    }
+}
+
+struct cryptoValueInfo: View {
+    
+    var cryptoName: String
+    var cryptoImage: String
+    var cryptoValue: Int
+    
+    var body: some View {
+        VStack{
+            Text(cryptoName)
+                .font(.title2)
+                .foregroundStyle(Color.white)
+            
+            Image(cryptoImage)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            
+            Text("$ \(cryptoValue)")
+        }
     }
 }
 
